fbfdcd92382a6aee3839c67cf8a57975ed7a34f0
Kevin van Rooijen <kevin.van.rooijen@gmail.com>
2017-02-14 14:21:07 +0200

    BUMP 0.1.8

--- mix.exs
+++ mix.exs
@@ -3,7 +3,7 @@ defmodule Lumber.Mixfile do
 
   def project do
     [app: :lumber,
-     version: "0.1.7",
+     version: "0.1.8",
      elixir: "~> 1.4",
      build_embedded: Mix.env == :prod,
      start_permanent: Mix.env == :prod,
